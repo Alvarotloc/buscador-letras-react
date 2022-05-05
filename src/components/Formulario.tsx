d1f278c6284db81945af15c9cb6f00dfb756ac74
Alvarotloc <alvarotloc@gmail.com>
2022-05-05 13:00:53 +0200

    Formulario añadido y añadido two way binding

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -1,7 +1,47 @@
-const Formulario = ():JSX.Element => {
+import { useState } from "react";
+const Formulario = (): JSX.Element => {
+  const [busqueda, setBusqueda] = useState({
+    artista: "",
+    cancion: "",
+  });
   return (
-    <div>Formulario</div>
-  )
-}
+    <form>
+      <legend>Busca por Artistas y Canción</legend>
+      <div className="form-grid">
+        <div>
+          <label htmlFor="artista">Artista</label>
+          <input
+            type="text"
+            name="artista"
+            placeholder="Nombre Artista"
+            value={busqueda.artista}
+            onChange={(evento) =>
+              setBusqueda({
+                ...busqueda,
+                [evento.target.name]: evento.target.value,
+              })
+            }
+          />
+        </div>
+        <div>
+          <label htmlFor="cancion">Canción</label>
+          <input
+            type="text"
+            name="cancion"
+            placeholder="Nombre Canción"
+            value={busqueda.cancion}
+            onChange={(evento) =>
+              setBusqueda({
+                ...busqueda,
+                [evento.target.name]: evento.target.value,
+              })
+            }
+          />
+        </div>
+        <input type="submit" value="Buscar" />
+      </div>
+    </form>
+  );
+};
 
-export default Formulario
+export default Formulario;
